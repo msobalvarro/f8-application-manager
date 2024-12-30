db9079409b3b9fe3417015d27ac632c3412435b8
Samuel Sobalvarro <dev.msobalvarro@gmail.com>
2024-12-30 15:15:48 -0600

    filtering message

--- app/message.tsx
+++ app/message.tsx
@@ -3,12 +3,19 @@ import { MessageCard } from '@/components/message/itemMessage'
 import { TitleView } from '@/components/TitleView'
 import { useAxios } from '@/hooks/useFetch'
 import { MessagesResponse } from '@/interfaces'
-import { MessageStyles as styles } from '@/styles'
-import { View } from 'react-native'
+import { MessageStyles as styles, UiStyles } from '@/styles'
+import { useState } from 'react'
+import { TextInput, View } from 'react-native'
 
 export default function MessageView() {
   const { data, isLoading, refetch } = useAxios<MessagesResponse[]>({ endpoint: '/message' })
+  const [filter, setFiter] = useState('')
 
+  const dataFilter = data?.filter((message) =>
+    `${message.fullName} ${message.email} ${message.phoneNumber}`
+      .toLowerCase()
+      .includes(filter.toLowerCase())
+  )
 
   return (
     <ContainerViewLayout scroll isLoading={isLoading} onRefresh={refetch}>
@@ -18,8 +25,15 @@ export default function MessageView() {
           title='Mensajes'
           subtitle='Recibe y administras a tus consultas y solicitudes' />
 
+        <TextInput
+          placeholder='Filtrar por Nombre, correo, número telefónico'
+          style={[UiStyles.InputStyle, { width: '100%' }]}
+          value={filter}
+          keyboardType='default'
+          onChangeText={setFiter} />
+
         <View style={{ gap: 20 }}>
-          {data?.map((data, i) => (<MessageCard refetch={refetch} message={data} key={i} />))}
+          {dataFilter?.map((data, i) => (<MessageCard refetch={refetch} message={data} key={i} />))}
         </View>
       </View>
     </ContainerViewLayout>
